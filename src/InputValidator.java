12c2a4b9c0bac5ce569d2735858a5a35be7fdc0f
Bssam111 <bssambnsih19@gmail.com>
2024-12-03 14:08:15 +0300

    v1

--- src/InputValidator.java
+++
@@ -1,9 +0,0 @@
-public class InputValidator {
-    public static boolean validateEmail(String email) {
-        return email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$");
-    }
-
-    public static boolean validatePhoneNumber(String phone) {
-        return phone.matches("\\+?[0-9]{10,15}");
-    }
-}
